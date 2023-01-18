2efa1f917b0e5fd2670b293a09b2e51d7787e039
anacarolcortez <anacarolcortez@hotmail.com>
2023-01-18 19:37:49 -0300

    ajuste endpoint resume e doc

--- src/main/java/com/ms/hireme/docs/SwaggerConfig.java
+++ src/main/java/com/ms/hireme/docs/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
         ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
 
         apiInfoBuilder.title("Hire Me: The Portfolio API");
-        apiInfoBuilder.description("API Rest using Java, Springboot, Validation, Security, DataBase (Postgres), Docker, RabbitMq. \nThe goal is to show what I can develop, as Associate Software Developer (entry level) so I can be hired by your company to join the team =) \nThe API is a show case of my own resume and allows any recruter or tech lead to register and set an interview appointment to get to know me better\nThe endpoints Candidate and Resume are only GET ones. A complete CRUD can be tested using other 2 endpoints: Registration and Interview \nGet the candidate's id (mine) to use it as parameter to the Resume endpoints and to the Interview");
+        apiInfoBuilder.description("API Rest using Java, Springboot, Validation, Security, DataBase (Postgres), Docker, RabbitMq. \nThe goal is to show what I can develop, as Associate Software Developer (entry level) so I can be hired by your company to join the team =) \nThe API is a show case of my own resume and allows any recruter or tech lead to register and set an interview appointment to get to know me better\nThe endpoint Resume is only a GET. A complete CRUD can be tested using other 2 endpoints: Registration and Interview. \nGet the candidate's id (mine) to use it as parameter to the Resume and Interview endpoints. \nAfter creating an interview request, we'll both receive an e-mail about it.");
         apiInfoBuilder.version("1.0");
         apiInfoBuilder.contact(this.contact());
 
--- src/main/java/com/ms/hireme/resume/controller/ResumeController.java
+++ src/main/java/com/ms/hireme/resume/controller/ResumeController.java
@@ -10,20 +10,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ms.hireme.resume.dto.CandidateDTO;
 import com.ms.hireme.resume.dto.CourseDTO;
 import com.ms.hireme.resume.dto.ExperienceDTO;
+import com.ms.hireme.resume.service.CandidateService;
 import com.ms.hireme.resume.service.CourseService;
 import com.ms.hireme.resume.service.ExperienceService;
 
 @RestController
-@RequestMapping("/resumes")
+@RequestMapping("/resume")
 public class ResumeController {
+
+    @Autowired
+    private CandidateService service;
 
     @Autowired
     private CourseService courseService;
 
     @Autowired
     private ExperienceService experienceService;
+
+    // In order to make it a 'metalinguistic' portfolio, there's only one candidate allowed in the list from DB: me ;)
+    @GetMapping("/candidates")
+    public ResponseEntity<List<CandidateDTO>> getCandidates(){
+        List<CandidateDTO> candidates = service.getCandidates();
+        return ResponseEntity.ok().body(candidates);
+    }
 
     @GetMapping("/courses/{id}")
     public ResponseEntity<List<CourseDTO>> getCoursesByCandidateId(@PathVariable UUID id){
